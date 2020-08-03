2b9ce0ff3733501b4b91575a585406a47bb75dcd
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-03 12:18:06 -0700

    [app.js] Finnally change negative values to red

--- project_1_cabassa/app.js
+++ project_1_cabassa/app.js
@@ -8,17 +8,20 @@ modifier.value = 1;
 
 plus.addEventListener("click", function (e) {
     display.textContent = display.innerText - (-modifier.value);
+    if (display.textContent < 0) {
+            display.style.color = "red";
+    } else {
+            display.style.color = "black";
+    }
 });
 
 minus.addEventListener("click", function (e) {
     display.textContent = display.textContent - modifier.value;
+    if (display.textContent < 0) {
+            display.style.color = "red";
+    } else {
+            display.style.color = "black";
+}
 });
-
-
-if (display.textContent < 0) {
-    h1.style.color = "red";
-} else {
-    h1.style.color = "black";
-};
 
 
